dda9a3405804df360c14076dfca164faeaf87f5c
Vivek Srivastava <vivek.srivastava@tal.com.au>
2021-05-26 15:14:38 +1000

    Initial Commit

---
+++ Algorithm-master/LinkedList/src/com/linklist/DoublyLinkList.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.linklist;
+
+/**
+ * @author Vivek Srivastava
+ *
+ */
+public class DoublyLinkList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
